5148478ca474fb72eabb2ba5434a0d2e9035dcfa
CDinuwan <chanukadinuwan35@gmail.com>
2021-05-06 15:29:36 +0530

    AI

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import playsound
 import os
 import random
 import gtts as gTTS
+import time
 
 from time import ctime
 
@@ -14,48 +15,50 @@ r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
-            jarvis_speak(ask)
+            print(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            jarvis_speak(voice_data)
         except sr.UnknownValueError:
-            jarvis_speak('Sorry,my speech service is down')
+            print("Sorry, I did not get that.")
+        except sr.RequestError:
+            print("Sorry, My speech service is down")
         return voice_data
 
 
-def jarvis_speak(audio_string):
-    tts = gTTS(text=audio_string, lang='en')
-    r = random.randint(1, 10000000)
-    audio_file = 'audio-'+str(r)+'.mp3'
-    tts.save(audio_file)
-    playsound.playsound(audio_file)
-    print(audio_string)
-    os.remove(audio_file)
-
-
 def respond(voice_data):
-    if 'What is your name' in voice_data:
-        jarvis_speak('My name is Jarvis')
-    if 'What time is it' in voice_data:
-        jarvis_speak(ctime())
+    if 'what is your name' in voice_data:
+        print("My name is Jarvis")
+    if 'what time is it' in voice_data:
+        print(ctime())
     if 'search' in voice_data:
-        search = record_audio('What do you want to search for?')
-        url = 'https://google.com/search?q=' + search
+        search = record_audio('What do you want to search for')
+        url = 'https://google.com/search?q='+search
         webbrowser.get().open(url)
-        jarvis_speak('Here is what I found for '+search)
+        print('Here is what I found for '+search)
     if 'find location' in voice_data:
-        location = record_audio('What is the location?')
-        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        location = record_audio('What is the location')
+        url = 'https://google.nl/maps/place/'+location+'/&amp;'
         webbrowser.get().open(url)
-        jarvis_speak('Here is the location of '+location)
+        print('Here is the location of ' + location)
     if 'exit' in voice_data:
         exit()
 
 
 time.sleep(1)
-jarvis_speak('How can I help you?')
+print("How can I help you?")
 while 1:
     voice_data = record_audio()
+    print(voice_data)
     respond(voice_data)
+
+
+# def jarvis_speak(audio_string):
+#     tts = gTTS(text=audio_string, lang='en')
+#     r = random.randint(1, 10000000)
+#     audio_file = 'audio-'+str(r)+'.mp3'
+#     tts.save(audio_file)
+#     playsound.playsound(audio_file)
+#     print(audio_string)
+#     os.remove(audio_file)
